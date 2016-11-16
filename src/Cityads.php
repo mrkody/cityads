e49db6a5f50f5c91b6eacfaa98b19fd5e46dc2c0
mrkody <kody1994@mail.ru>
2016-11-16 17:04:30 +0300

    first

---
+++ src/Cityads.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mrkody\Cityads;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
+use Exception;
+
+/**
+ * Class Cityads
+ *
+ * @author mrkody kody1994@mail.ru
+ *
+ * @package Mrkody\Cityads
+ */
+
+class Cityads {
+
+	public function get(){
+
+	}
+
+}
